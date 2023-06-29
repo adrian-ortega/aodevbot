1019b1bca5825939a3876b31f6cc3f70b6a9e492
Adrian Ortega <hello@aortegadesign.com>
2023-06-29 16:35:34 -0700

    Updates Stream Model definition.

--- src/app/models/Stream.js
+++ src/app/models/Stream.js
@@ -3,10 +3,10 @@ const StreamModel = (sequelize, Sequelize) => {
   const Stream = sequelize.define('Stream', {
     id: { type: INTEGER, primaryKey: true, autoIncrement: true },
     stream_id: { type: BIGINT, primaryKey: true },
+    game_id: { type: BIGINT, primaryKey: true },
     title: { type: STRING },
-    mature: { type: BOOLEAN, defaultValue: false },
-    start_time: { type: DATE },
-    end_time: { type: DATE }
+    started_at: { type: DATE },
+    ended_at: { type: DATE }
   });
   return Stream;
 };
