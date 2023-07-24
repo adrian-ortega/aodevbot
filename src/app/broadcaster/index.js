76f9026817d2e0bce63d7815ec8acd1b52dea29c
Adrian Ortega <hello@aortegadesign.com>
2023-07-23 17:54:48 -0700

    Adds error to getBroadcaster helper

--- src/app/broadcaster/index.js
+++ src/app/broadcaster/index.js
@@ -1,3 +1,9 @@
 const { Chatters } = require('../models');
 
-exports.getBroadcaster = () => Chatters.findOne({ where: { broadcaster: true } });
+exports.getBroadcaster = async () => {
+  const Broadcaster = await Chatters.findOne({ where: { broadcaster: true } });
+  if (!Broadcaster) {
+    throw new Error('No broadcaster!')
+  }
+  return Broadcaster;
+}
