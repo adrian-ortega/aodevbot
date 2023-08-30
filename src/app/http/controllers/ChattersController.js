c9853b21d48e7ffd1ccec5edd72a7542fb9d0e31
Adrian Ortega <adrian@aortegadesign.com>
2023-08-30 10:09:19 -0700

    Updates chatters controller to include sync, sync status and better list params

--- src/app/http/controllers/ChattersController.js
+++ src/app/http/controllers/ChattersController.js
@@ -1,4 +1,4 @@
-const { Chatters, Sequelize } = require("../../models");
+const { Chatters, KeyValue, Sequelize } = require("../../models");
 
 const chattersTraformer = (row) => {
   return {
@@ -17,27 +17,37 @@ const chattersTraformer = (row) => {
 exports.list = async (req, res) => {
   const { search } = req.query;
   let { page, limit } = req.query;
+  page = page ? parseInt(page, 10) : 1;
+  const attrs = {}
+  attrs.limit = !isNaN(limit) && isFinite(limit) ? parseInt(limit, 10) : 10;
+  attrs.offset = (page - 1) * attrs.limit
   let data = [];
 
-  page = page ? parseInt(page, 10) : 1;
-  limit = !isNaN(limit) && isFinite(limit) ? parseInt(limit, 10) : 10;
-  const offset = (page - 1) * limit;
-  const pagination = {};
-  const where = {};
-
-  if (search && search.length > 0) {
-    where[Sequelize.Op.or] = [
-      { username: { [Sequelize.Op.like]: `%${search}%` } },
-      { display_name: { [Sequelize.Op.like]: `%${search}%` } },
-    ];
+  if (attrs.limit === -1) {
+    delete attrs.limit;
+    delete attrs.offset;
+  }
+  if (attrs.limit < 0) {
+    attrs.limit = 10;
   }
 
-  data = await Chatters.findAndCountAll({ where, limit, offset });
+  const pagination = {};
+
+  if (search && search.length > 0) {
+    attrs.where = {
+      [Sequelize.Op.or]: [
+        { username: { [Sequelize.Op.like]: `%${search}%` } },
+        { display_name: { [Sequelize.Op.like]: `%${search}%` } },
+      ]
+    }
+  }
+
+  data = await Chatters.findAndCountAll(attrs);
   const rows = data.rows.map(chattersTraformer);
 
   pagination.page = page;
-  pagination.limit = limit;
-  pagination.pages = Math.ceil(data.count / limit);
+  pagination.limit = attrs.limit ? attrs.limit : -1;
+  pagination.pages = !attrs.limit ? 1 : Math.ceil(data.count / attrs.limit);
   pagination.total = data.count;
 
   res.send({
@@ -137,13 +147,48 @@ exports.destroy = (req, res) => {
     });
 };
 
+const getCurrentSyncStats = async (total = null) => {
+  const [model] = await KeyValue.findOrCreate({
+    where: {
+      item_key: 'chatters_sync'
+    },
+    defaults: {
+      item_key: 'chatters_sync',
+      item_value: {
+        total,
+        current: 0,
+        percentage: 0
+      }
+    }
+  });
+
+  if (total !== null) {
+    model.update({
+      item_value: {
+        total,
+        current: 0,
+        percentage: 0
+      }
+    })
+  }
+
+  return model;
+}
 exports.sync = async (req, res) => {
   const Twitch = require("../../twitch");
-  const chatters = await Twitch.getFollowers()
-  console.log(`Found ${chatters.length} Chatters`)
-  const syncData = []
+  let syncTotal = 1;
+  const stats = await getCurrentSyncStats(0);
+  const statUpdate = async (current = 0, percentage = 0) => {
+    return stats.update({
+      item_value: { total: syncTotal, current, percentage }
+    })
+  }
+  await statUpdate()
 
-  console.log('Pulling data from Twitch')
+  const chatters = await Twitch.getFollowers()
+  const syncData = []
+  syncTotal = chatters.length
+
   for (let i = 0; i < chatters.length; i++) {
     const item = await Twitch.getUser(chatters[i].from_id);
     syncData.push({
@@ -152,9 +197,8 @@ exports.sync = async (req, res) => {
       display_name: item.display_name,
       profile_image_url: item.profile_image_url
     })
+    await statUpdate(i + 1, 100 * (i / syncTotal))
   }
-
-  console.log('Attempting Bulk Create')
 
   const results = await Chatters.bulkCreate(syncData, {
     fields: ['twitch_id', 'username', 'display_name', 'profile_image_url'],
@@ -162,5 +206,14 @@ exports.sync = async (req, res) => {
     upsertOn: ['twitch_id']
   })
 
+  await statUpdate(syncTotal, 100)
   res.send({ results });
 };
+
+exports.syncStatus = async (req, res) => {
+  const stats = await getCurrentSyncStats();
+  res.send({
+    ...stats.item_value,
+    updated: stats.updated_at
+  })
+}
