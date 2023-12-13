1f8d4b0e22c1eb8908eba7d8a9015352140245ad
Adrian Ortega <hello@aortegadesign.com>
2023-12-13 09:56:22 -0800

    implements SongName !song command

--- src/app/spotify/chat/commands/SongNameCommand.js
+++ src/app/spotify/chat/commands/SongNameCommand.js
@@ -1,4 +1,6 @@
 const spotify = require("../../../spotify");
+const { isEmpty, randomFromArray } = require("../../../support");
+const { botMessageReply } = require("../../../twitch/chat/commands");
 const { USER_DISPLAY_NAME } = require("../../../twitch/chat/state-keys");
 
 exports.name = () => {
@@ -16,7 +18,16 @@ exports.handle = async (message, state, channel, { client }, resolve) => {
   try {
     const username = state[USER_DISPLAY_NAME];
     const data = await spotify.getCurrentlyPlaying();
-    console.log(username, data)
+    if(isEmpty(data)) {
+      return client.say(channel, `Sorry ${username}, it looks like nothing is playing right now.`)
+    }
+    const song = data.item.name;
+    const artist = data.item.artists ? ` | ${data.item.artists.map((artist) => artist.name).join(', ')}` : '';
+    const savedTracks = await spotify.checkSavedTracks(data.item.id);
+    const icon = savedTracks.length && savedTracks[0]
+      ? randomFromArray(['❤️', '🧡', '💛', '💚', '💙', '💜', '🖤', '🤎', '🤍', '💕'])
+      : randomFromArray(['🎵', '🎶', '🔊', '🎧']);
+    client.say(channel, botMessageReply(`${icon} Playing: ${song}${artist}`));
   } catch (err) {
     
   }
