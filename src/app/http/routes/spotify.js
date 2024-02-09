7c4fb5f226f34c3af73cd926a1b0e0a18751cfd8
Adrian Ortega <hello@aortegadesign.com>
2024-02-09 11:23:40 -0800

    removes downloader, scrapped idea

--- src/app/http/routes/spotify.js
+++ src/app/http/routes/spotify.js
@@ -1,10 +1,7 @@
 const router = require("express").Router();
 const authController = require("../controllers/SpotifyAuthController");
-const downloadController = require('../controllers/SpotifyDownloaderController');
 
 router.get("/authenticate", authController.authenticate);
 router.get("/authenticate/confirm", authController.authConfirm);
-
-router.get('/downloader', downloadController.downloader);
 
 module.exports = router;
